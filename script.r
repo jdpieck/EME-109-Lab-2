7ff294f134f270f6bab1f9186b9e025a6ddd9eb7
jdpieck <jdpieck@ucdavis.edu>
2026-05-18 21:28:37 -0700

    locking in

--- script.r
+++ script.r
@@ -48,8 +48,35 @@ c_p_inv   <- 1 - 4 * (sin(angle_rad))^2
 
 prob_2 <- data.frame(
 pressure_tap_angle = drop_units(cylinder_data$pressure_tap_angle),
-  c_p_exp                = drop_units(c_p_exp),
-  c_p_inv                = c_p_inv
+  c_p_exp          = drop_units(c_p_exp),
+  c_p_inv          = c_p_inv
 )
 
 write_json(prob_2, "c_p.json", dataframe = "columns", pretty = TRUE)
+
+
+## Prob 3
+
+cylinder_radius <- set_units(31.8, "mm")  
+cylinder_length <- set_units(300, "mm")
+
+integral_sum <- set_units(0, "Pa")
+
+for (i in 1:(nrow(cylinder_data) - 1)) {
+  d_theta <- angle_rad[i + 1] - angle_rad[i]
+  
+  avg_height <- (cylinder_data$cylinder_manometer[i] * cos(angle_rad[i]) + cylinder_data$cylinder_manometer[i + 1] * cos(angle_rad[i + 1])) / 2
+  d_pressure <- avg_height * d_theta
+  
+  integral_sum <- integral_sum + d_pressure
+}
+
+print(integral_sum)
+D_p = 2 * cylinder_length * cylinder_radius * integral_sum
+units(D_p) <- "N"
+print(D_p)
+
+given_drag_coef <- 1.2
+F_d = given_drag_coef * air_density / 2 * u_infinity ^2 * cylinder_radius *2 * cylinder_length # table 7-3, page 494
+units(F_d) <- "N"
+print(F_d)
